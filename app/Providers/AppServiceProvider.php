599fa9a7b466512f63687c2184b929016fd71aab
victor braz <victor.90braz@gmail.com>
2023-12-04 15:32:29 +0100

    rename to correct import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
                 ->numbers()
                 ->symbols()
                 ->mixedCase()
+                ->uncompromised()
         );
     }
 }

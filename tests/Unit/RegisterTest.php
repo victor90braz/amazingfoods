57f5d9931bfeb82e40bf628ce640c8c4849b8849
victor braz <victor.90braz@gmail.com>
2023-12-04 16:38:50 +0100

    test register controller

--- tests/Unit/RegisterTest.php
+++ tests/Unit/RegisterTest.php
@@ -2,24 +2,28 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Validation\Rules\Password;
+use Tests\TestCase;
 
 class RegisterTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_registers_a_user_successfully(): void
     {
-        $response = $this->post('/register', [
-            'fullName' => 'victor',  // Corrected field name
+        $response = $this->post('/register/store', [
+            'fullName' => 'victor',
             'email' => 'test@example.com',
-            'password' => 'password123',
+            'password' => 'password123!P',
         ]);
-
-        $response->assertRedirect('/login');
+        $response->assertStatus(302); // Check for a redirect status
+        $response->assertRedirect('/');
+        $this->assertDatabaseHas('users', ['email' => 'test@example.com']);
     }
 }
